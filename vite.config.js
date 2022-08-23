9f1afefd0704a511efd1b31786d4efdcf62272d0
Kevin <kevinbalicot@gmail.com>
2022-08-23 11:30:10 +0200

    chore: Update vitejs config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    manifest: true,
-  }
+export default defineConfig(({ mode }) => {
+    const { NODE_PORT, NODE_ENV } = loadEnv(mode, process.cwd(), '');
+    const config = {
+        plugins: [vue()],
+        build: {
+            manifest: true,
+        },
+        mode: NODE_ENV
+    }
+
+    if ('dev' === NODE_ENV) {
+        config.server = {
+            proxy: {
+                '/info': `http://web:${NODE_PORT}`,
+                '^/api/.*': `http://web:${NODE_PORT}`,
+            }
+        }
+    }
+
+    return config;
 });
